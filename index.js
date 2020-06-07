2156437e807c84ef311ea4cad25d8b845008c135
Jaime385 <47996736+jaime385@users.noreply.github.com>
2020-06-07 16:41:25 -0600

    Changing the port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('./client/build'));
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`Server started on port ${port}`) });
 
 //Connect to mongoDB
